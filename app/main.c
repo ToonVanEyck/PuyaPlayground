7e5caa8c07206a8bfc06081e838f1a5d0203e2db
Toon Van Eyck <toonvaneyck@gmail.com>
2024-03-14 22:53:21 +0000

    added adc based absolute encoder readout

--- app/main.c
+++ app/main.c
@@ -21,17 +21,25 @@
  */
 
 /* Includes ------------------------------------------------------------------*/
+#include "SEGGER_RTT.h"
 #include "py32f0xx_bsp_printf.h"
 #include "py32f0xx_hal.h"
 
-/* Private define ------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private user code ---------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-void APP_ErrorHandler(void);
+#include <stdint.h>
 
 /* Private define ------------------------------------------------------------*/
+#define NUM_CHARS 48
+
+#define GPIO_PIN_LED GPIO_PIN_7
+#define GPIO_PORT_LED GPIOA
+#define GPIO_PIN_MOTOR GPIO_PIN_6
+#define GPIO_PORT_MOTOR GPIOA
+
+typedef struct {
+    uint16_t ir_low;
+    uint16_t ir_high;
+} ir_sens_limits_t;
+
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef AdcHandle;
 ADC_ChannelConfTypeDef sConfig;
@@ -40,10 +48,18 @@ TIM_HandleTypeDef TimHandle;
 TIM_MasterConfigTypeDef sMasterConfig;
 TIM_OC_InitTypeDef sConfigOC;
 UART_HandleTypeDef UartHandle;
+ir_sens_limits_t ir_limits[6] = {
+    {.ir_low = 1900, .ir_high = 2100}, {.ir_low = 1900, .ir_high = 2100}, {.ir_low = 1900, .ir_high = 2100},
+    {.ir_low = 1900, .ir_high = 2100}, {.ir_low = 1900, .ir_high = 2100}, {.ir_low = 1900, .ir_high = 2100},
+};
+uint8_t ir_map[6] = {2, 3, 1, 4, 0, 5};
+uint8_t enc_grey = 0;
+uint8_t enc_dec = 0;
 
-/* Private user code ---------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
+
 /* Private function prototypes -----------------------------------------------*/
+void APP_ErrorHandler(void);
 static void APP_LedConfig(void);
 static void APP_TimerInit(void);
 static void APP_AdcConfig(void);
@@ -67,7 +83,14 @@ int main(void)
     // APP_UartInit();
 
     printf("Started Puya App\r\n");
+    SEGGER_RTT_WriteString(0, "Started Puya App\r\n");
+    uint8_t new_dec = 0xff;
     while (1) {
+        if (new_dec != enc_dec) {
+            new_dec = enc_dec;
+            SEGGER_RTT_printf(0, "encoder:  %d\r\n", new_dec);
+        }
+        HAL_Delay(100);
         // uint8_t buffer[4];
         // HAL_UART_Receive(&UartHandle, buffer, sizeof(buffer), HAL_MAX_DELAY);
         // HAL_UART_Transmit(&UartHandle, buffer, sizeof(buffer), HAL_MAX_DELAY);
@@ -82,13 +105,14 @@ static void APP_LedConfig(void)
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOF_CLK_ENABLE();
 
-    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    GPIO_InitStruct.Pin = GPIO_PIN_LED;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    GPIO_InitStruct.Pin = GPIO_PIN_12;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIO_PORT_LED, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = GPIO_PIN_MOTOR;
+    HAL_GPIO_Init(GPIO_PORT_MOTOR, &GPIO_InitStruct);
+    HAL_GPIO_WritePin(GPIO_PORT_MOTOR, GPIO_PIN_MOTOR, GPIO_PIN_RESET);
 }
 
 static void APP_AdcConfig(void)
@@ -144,17 +168,17 @@ static void APP_AdcConfig(void)
         APP_ErrorHandler();
     }
     /* Start ADC with DMA */
-    if (HAL_ADC_Start_DMA(&AdcHandle, aADCxConvertedData, 6) != HAL_OK) {
-        APP_ErrorHandler();
-    }
+    // if (HAL_ADC_Start_DMA(&AdcHandle, aADCxConvertedData, 6) != HAL_OK) {
+    //     APP_ErrorHandler();
+    // }
 }
 
 static void APP_TimerInit(void)
 {
-    // (800 * 1000) / 8Mhz = 100ms
+    // (800 * 1000) / 8Mhz = 1ms
     __HAL_RCC_TIM1_CLK_ENABLE();
     TimHandle.Instance = TIM1;
-    TimHandle.Init.Period = 1000 - 1;
+    TimHandle.Init.Period = 10 - 1;
     TimHandle.Init.Prescaler = 800 - 1;
     TimHandle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     TimHandle.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -173,17 +197,17 @@ static void APP_TimerInit(void)
         APP_ErrorHandler();
     }
 
-    sConfigOC.OCMode = TIM_OCMODE_TOGGLE;
-    sConfigOC.Pulse = 5000;
-    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    // sConfigOC.OCMode = TIM_OCMODE_TOGGLE;
+    // sConfigOC.Pulse = 5000;
+    // sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
 
-    if (HAL_TIM_OC_ConfigChannel(&TimHandle, &sConfigOC, TIM_CHANNEL_4) != HAL_OK) {
-        APP_ErrorHandler();
-    }
+    // if (HAL_TIM_OC_ConfigChannel(&TimHandle, &sConfigOC, TIM_CHANNEL_4) != HAL_OK) {
+    //     APP_ErrorHandler();
+    // }
 
-    if (HAL_TIM_OC_Start_IT(&TimHandle, TIM_CHANNEL_4) != HAL_OK) {
-        APP_ErrorHandler();
-    }
+    // if (HAL_TIM_OC_Start_IT(&TimHandle, TIM_CHANNEL_4) != HAL_OK) {
+    //     APP_ErrorHandler();
+    // }
 }
 
 static void APP_DmaInit(void)
@@ -225,15 +249,53 @@ static void APP_UartInit(void)
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
 {
-    // aADCxConvertedData = hadc->Instance->DR;
-    printf("ADC: %04ld  %04ld  %04ld  %04ld  %04ld  %04ld\r\n", aADCxConvertedData[0], aADCxConvertedData[1],
-           aADCxConvertedData[2], aADCxConvertedData[3], aADCxConvertedData[4], aADCxConvertedData[5]);
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_12);
+    // SEGGER_RTT_printf(0, "ADC: %04ld  %04ld  %04ld  %04ld  %04ld  %04ld\r\n", aADCxConvertedData[ir_map[0]],
+    //                   aADCxConvertedData[ir_map[1]], aADCxConvertedData[ir_map[2]], aADCxConvertedData[ir_map[3]],
+    //                   aADCxConvertedData[ir_map[4]], aADCxConvertedData[ir_map[5]]);
+
+    /* Convert ADC result into grey code. */
+    for (uint8_t i = 0; i < 6; i++) {
+        if (aADCxConvertedData[ir_map[i]] > ir_limits[i].ir_high) {
+            enc_grey &= ~(1 << i);
+        } else if (aADCxConvertedData[ir_map[i]] < ir_limits[i].ir_low) {
+            enc_grey |= (1 << i);
+        }
+    }
+
+    /* Disable IR led. */
+    HAL_GPIO_WritePin(GPIO_PORT_LED, GPIO_PIN_LED, GPIO_PIN_RESET);
+
+    /* Halt ADC with DMA. */
+    if (HAL_ADC_Stop_DMA(&AdcHandle) != HAL_OK) {
+        APP_ErrorHandler();
+    }
+
+    /* Convert grey code into decimal. */
+    uint8_t enc_g = enc_grey;
+    uint8_t enc_d = 0;
+    for (enc_d = 0; enc_g; enc_g = enc_g >> 1) {
+        enc_d ^= enc_g;
+    }
+    uint8_t new_dec = (uint8_t)NUM_CHARS - enc_d - 1;
+    if (new_dec < NUM_CHARS) {
+        enc_dec = new_dec;
+    }
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
+    static uint16_t period_step_cnt = 0;
+    period_step_cnt++;
+    if (period_step_cnt == 2) {
+        /* Start ADC with DMA */
+        if (HAL_ADC_Start_DMA(&AdcHandle, aADCxConvertedData, 6) != HAL_OK) {
+            APP_ErrorHandler();
+        }
+
+    } else if (period_step_cnt >= 10) {
+        period_step_cnt = 0;
+        HAL_GPIO_WritePin(GPIO_PORT_LED, GPIO_PIN_LED, GPIO_PIN_SET);
+    }
 }
 
 // void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
